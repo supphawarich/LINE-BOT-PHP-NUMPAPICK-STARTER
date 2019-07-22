71a06d2529c797a3c11b235d9254854f3c76774b
supphawarich <53190464+supphawarich@users.noreply.github.com>
2019-07-23 01:16:40 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'ZO5quYrOa3egCUMEUs2YiTn5VCUvqlfuufPCPfPHqfk0OisIELwlY8EPpAipzRQJSP/tKogHL1YP4XqqwnAdlha4ei6ZZKlvlgmzlsudB42+pgpZ4qWgJLWhtEHBYtcMxdiqhRRVeB8rwP+3eTTnMQdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Ud8529c85198d15b0c6b046d627c9a55b',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
